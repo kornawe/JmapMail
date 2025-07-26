8b3b60416c8d7e3bcd480eeb76587db29c5cc087
BKey <kornawe@gmail.com>
2025-07-26 11:25:47 -0400

    Basic Infra

--- infra/src/InfraStack.cs
+++ infra/src/InfraStack.cs
@@ -1,13 +1,37 @@
 using Amazon.CDK;
 using Constructs;
 
-namespace Infra
+namespace Infra;
+
+public class InfraStack : Stack
 {
-    public class InfraStack : Stack
+    private string EnvironmentName { get; }
+    
+    internal InfraStack(Construct scope, string envName, IStackProps props = null) : base(scope, $"JmapMail-{envName}", props)
     {
-        internal InfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        EnvironmentName = envName;
+
+        // S3 bucket with unique name per environment
+        var bucket = new Bucket(this, $"mail-bucket-{EnvironmentName}", new BucketProps
         {
-            // The code that defines your stack goes here
-        }
+            BucketName = $"mail-bucket-{EnvironmentName}-{Account}-{Region}".ToLower(),
+            Versioned = true,
+            RemovalPolicy = RemovalPolicy.DESTROY
+        });
+
+        // DynamoDB table with unique name per environment
+        new Table(this, $"MailMetadata-{EnvironmentName}", new TableProps
+        {
+            TableName = $"MailMetadata-{EnvironmentName}",
+            PartitionKey = new Attribute
+            {
+                Name = "MessageId",
+                Type = AttributeType.STRING
+            },
+            BillingMode = BillingMode.PAY_PER_REQUEST,
+            RemovalPolicy = RemovalPolicy.DESTROY
+        });
+
+        
     }
 }
